2df46b048f6c173fcb84adf0fe78f767bb0845fa
Jonathan Steel <jonathansteel4@gmail.com>
2020-07-26 10:58:32 +0200

    Addded database functionality from Corey Schafer tutorial, needs modification but does the trick.

--- quiz_program.py
+++ quiz_program.py
@@ -4,11 +4,46 @@ from flask import Flask, render_template, url_for, flash, redirect
 # Importing the necessary forms
 from forms import RegistrationForm, LoginForm
 
+# Importing SQL Alchemy
+from flask_sqlalchemy import SQLAlchemy
+
+# Other imports
+from datetime import datetime
+
 # Initialising the App
 app = Flask(__name__)
 
 # A secret key for encryption to protect against hacking
 app.config['SECRET_KEY'] = 'b84fd909956dc9ecfc1f258078ebe953'
+
+# References the location of the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+# An instance of an SQLAlchemy database
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
 
 # Creating the Home Page
 # The following links redirect the user to the home.html file
